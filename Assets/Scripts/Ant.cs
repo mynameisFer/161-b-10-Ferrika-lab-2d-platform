bdc2fb8df3bd37dfc7f4daac6c339af493929e92
mynameisFer <cocoferrika2549@gmail.com>
2025-10-21 15:36:35 +0700

    add  pointA,pointB of ant walk

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class Ant : Enemy
 {
+    [SerializeField] Vector2 velocity;
+    public Transform[] MovePoint;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(30);
+    }
+
+    public override void Behavior()
+    {
+        throw new System.NotImplementedException();
     }
 
     // Update is called once per frame
